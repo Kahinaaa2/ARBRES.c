348df614c0c9d9be3ccbcaf37d042d724ab06930
HAMMAD Kahina <166177981+Kahinaaa2@users.noreply.github.com>
2024-11-03 15:51:01 +0100

    Create Exercice1.c

---
+++ Exercice1.c
@@ -0,0 +1,13 @@
+Soit l'arbre suivant :....
+  donner :
+l'ordre de l'arbre : 4
+  le degres du noeud 4: 2 (nombre de fils)
+  le degres du noeud 2: 3
+
+il y a 11 feuilles, noeud sans fils
+l'arbre est de hauteur 3 (racine de hauteur 0)
+
+  parcours prefixe:
+1 2 7 8 15 16 9 4 10 12 6 13 14 17 18 19 20 14
+  parcorur en largeur:
+1 2 4 6 7 8 9 10 12 13 14 15 16 17 18 19 20
